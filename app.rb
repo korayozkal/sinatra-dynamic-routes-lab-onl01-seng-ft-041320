d7c38cf6d211c300e7d7ff222e7a57182b7eb973
Koray Ozkal <koryozkal@gmail.com>
2020-06-08 05:53:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,6 +27,17 @@ end
       "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
     end
     
-    
+    get '/:operation/:number1/:number2' do 
+    @number_1 = params[:number1].to_i
+    @number_2 = params[:number2].to_i
+    if params[:operation] == "add"
+      "#{@number_1+@number_2}"
+    elsif params[:operation] == "subtract"
+      "#{@number_1-@number_2}"
+    elsif params[:operation] == "multiply"
+      "#{@number_1*@number_2}"
+    elsif params[:operation] == "divide"
+      "#{@number_1/@number_2}"
+    end
 end
 
